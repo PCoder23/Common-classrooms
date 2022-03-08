f7a5714d7e9cb7fe15eaed8cbb11f502e9814278
Soumali <gorai.soumali@gmail.com>
2022-03-08 20:50:31 +0530

    Teaching debdeep conditional rendering

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const Btn = styled.button`
-position: relative;
-border-radius: 10px;
-border-color: #d9c46a;
-left: 83vw;
-height: 3vw;
-width: 6vw;
-margin: 10px 10px;
-box-shadow: -2px 1px #5b5d61;
-background-color: ${(props) => (props.name === "Login" ? "#e3c968" : "#d9c46a")};
-`
+  position: relative;
+  border-radius: 10px;
+  border-color: #d9c46a;
+  left: 83vw;
+  height: 3vw;
+  width: 6vw;
+  margin: 10px 10px;
+  box-shadow: -2px 1px #5b5d61;
+  background-color: ${(props) =>
+    props.name === "Login" ? "#e8e19e" : "#cfb563"};
+  cursor: pointer;
+`;
 
-function Button(props){
-    return <Btn>{props.name}</Btn>
+function Button(props) {
+  return <Btn name={props.name}>{props.name}</Btn>;
 }
 
 export default Button;
