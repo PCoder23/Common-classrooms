49ae0adbac7678f1fd2f1f78e55552cb1753f889
Debdeep1 <deb.baner.1@gmail.com>
2022-03-08 19:55:33 +0530

    Added little bit design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const MainContainer = styled.div`
     width: 100%;
     height:100vh;
-    
-    
+    opacity: 0.35;
+    background-color: #F4E877;
 `
 
 const Main = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+
+
+
+const NavColor= styled.div`
+background-color: #d9c46a;
+opacity: 1;
+height: 3.9rem;
+`
+
 
 const Navbar = () => {
   return (
-    <div>
-      <button>Login</button>
-      <button>SignIn</button>
-    </div>
+    <NavColor>
+    <Button name="Login"/>
+    <Button name="SignUp"/>
+    </NavColor>
   );
 };
 
