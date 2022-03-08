a22c9982bec17ec0aa10a7cc986b87259b8439ee
Soumali <gorai.soumali@gmail.com>
2022-03-08 18:48:00 +0530

    Adding the basic structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    width: 100%;
+    height:100vh;
+    
+    
+`
+
+const Main = () => {
+    return(
+        <MainContainer>
+
+        </MainContainer>
+    );
+}
+
+export default Main;
